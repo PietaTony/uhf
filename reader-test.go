ee469915cb5f1d4cfbd381240d503a140318bad3
PietaLiu <sjmtony@gmail.com>
2019-08-23 21:35:50 +0800

    8/23 3

---
+++ reader-test.go
@@ -0,0 +1,28 @@
+package main
+
+import "UHFRFID/reader"
+import "time"
+
+func main(){
+	UHFRFID.Begin("COM11", 57600)
+	adr := uint8(0x00)
+
+	/*
+	EPCData := []uint8{0xE2, 0x00, 0x00, 0x1B, 0x41, 0x15, 0x02, 0x23, 0x16, 0x20, 0xC9, 0x4B}
+	EPC := Memory{Len: (uint8)(len(EPCData)/2), Data: EPCData,}
+	pwd := Memory{Data: []uint8{0x00, 0x00},}
+	tag := Tag{EPC: EPC, Pwd: pwd,}
+
+	specData := Memory{Len: 0x04,}
+	spec := Spec{Name: UserMem, Adr: 0x00, Mem: specData,}
+
+	mask := Mask{Adr: 0x00, Len: 0x00,}
+
+	//PrintBytes(ReadData(adr, tag, spec, mask))
+	*/
+	for {
+    	UHFRFID.InventoryAll(adr)
+    	time.Sleep(100 * time.Millisecond)
+	}
+    defer UHFRFID.Close()
+}
